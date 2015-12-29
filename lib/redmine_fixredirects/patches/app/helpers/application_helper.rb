09eb3d6554c9fd146a888197c131f09cec845a96
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-12-29 09:20:30 +0300

    Fixed error 500

--- lib/redmine_fixredirects/patches/app/helpers/application_helper.rb
+++ lib/redmine_fixredirects/patches/app/helpers/application_helper.rb
@@ -17,6 +17,8 @@ module FixRedirects
 
 		module InstanceMethods
 			def context_menu_with_fixredirects(url)
+				return context_menu_without_fixredirects(url) if back_url.nil?
+
 				url_parts = url.split("?")
 				url_fixed = "#{url_parts[0]}?next=#{URI.encode(back_url, /\W/)}#{url_parts[1].nil? ? "" : "&#{url_parts[1]}"}";
 				return context_menu_without_fixredirects(url_fixed)
